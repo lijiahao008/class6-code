2a2aaa138ff299e787c86b674950ef4fb74bcc00
lijiahao008 <lijiahao008@users.noreply.github.com>
2015-02-27 22:39:18 -0500

    Update Deck.java

--- WarAndPeaceSkeleton/src/Deck.java
+++ WarAndPeaceSkeleton/src/Deck.java
@@ -1,14 +1,42 @@
+import java.lang.Math;
 
 
 public class Deck {
 
-	public Deck() {
+    private Card[] deck = new Card[52];
+    private int firstCard;
 
-	}
+    Deck() {
 
-	public Card draw() {
+        firstCard = 0;
+        
+        for (int i = 0; i < deck.length; i++)
+            deck[i] = new Card(i);
 
-	}
+    }
 
+    public  void Random() {
 
+        firstCard = 0;
+
+        for (int i = 0; i < 100; i++) {
+            int j =  (int) (Math.random() % 52);
+            int k =  (int) (Math.random() % 52);
+            Card topCard = deck[j];
+            deck[j] = deck[k];
+            deck[k] = topCard;
+        } 
+   }
+
+    public Card Draw() {
+        Card theCard;
+        if (firstCard < deck.length) {
+            theCard = deck[firstCard];
+            firstCard++;
+        }
+        else
+            theCard = null;
+
+        return theCard;
+    }
 }
